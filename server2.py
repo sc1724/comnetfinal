650dab01245f04687fadf2e0373b6f0f90748356
sc1724 <43208346+sc1724@users.noreply.github.com>
2021-05-06 13:59:48 -0400

    Update server2.py

--- server2.py
+++ server2.py
@@ -13,5 +13,5 @@ class udpserver():
 #Initializes new server and sets it up to receive packets
 if __name__ == '__main__':
         print("Server Start...")
-        udp_server = udpserver(id=103, ip='192.168.3.1', gateway=('192.168.2.2',8885), port=8887)
+        udp_server = udpserver(id=103, ip='192.168.3.1', gateway=('192.168.2.2',8874), port=8875)
         receive_packet(udp_server, None)
